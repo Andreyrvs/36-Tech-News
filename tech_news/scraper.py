99f74a0432114acfe330a4636811bab9cc302f6e
Andrey Visniewski <cortaesai@gmail.com>
2023-01-23 21:16:32 -0300

    fix: Url, title, summary, category - correspongem a pagina da noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,27 +37,34 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_news(html_content):
     selector = Selector(html_content)
-    url = selector.css("div.cs-overlay a::attr(href)").get()
-    title = selector.css("h2.entry-title a::text").get()
+
+    url = selector.xpath('//link[@rel="canonical"]/@href').get()
+    title = selector.css("h1.entry-title::text").get()
     timestamp = selector.css("ul.post-meta li::text").get()
     writer = selector.css("span.author a::text").get()
+
     comments_count = selector.css("div.post-comments h5::text").get()
     if comments_count is None:
         comments_count = 0
-    summary = selector.css("div.entry-excerpt::text").get()
+
+    summary = selector.xpath('//div[@class="entry-content"]/p[1]').get()
+    summary = selector.xpath('string(//p[1])').getall()
+    summary_formatted = " ".join(summary)
+
     tags = selector.xpath("//section.post-tags/ul/li/a/text()").getall()
-    category = selector.css("a.category-style span.label::text").get()
+
+    category = selector.css("span.label::text").get()
     dicio = {
         "url": url,
         "title": str(title).rstrip(),
         "timestamp": timestamp,
         "writer": writer,
         "comments_count": int(comments_count),
-        "summary": str(summary).strip(),
+        "summary": str(summary_formatted).strip(),
         "tags": tags,
         "category": str(category),
     }
-    print(f"🔥🔥🔥{dicio}")
+    # print(f"🔥🔥🔥{summary_first}")
     return dicio
 
 

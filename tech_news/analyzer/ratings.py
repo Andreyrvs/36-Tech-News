03830802c1d3bb730f617ae09ea2220b58cf18cd
Andrey Visniewski <cortaesai@gmail.com>
2023-01-30 20:44:53 -0300

    fix: (Requisito 10) - Retorna os  5 maiores comentarios

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -6,27 +6,13 @@ def top_5_news():
     sorted_ratings = []
 
     result = find_news()
-    print('result: ', result)
-    for doc in find_news():
-        # doc['comments_count'].sort(revense=True)
-        sorted_ratings.append({doc["title"], doc["url"], doc['comments_count']})
-        # print(doc['comments_count'])
 
-    # print(sorted(sorted_ratings, key=sorted_ratings['comments_count']))
-    # list_tuple = []
-    # for element in result:
-    #     # sorted_ratings.append(element["comments_count"])
-    #     # sorted_ratings.
-    #     # if sorted_ratings == element["comments_count"]:
-    #     # list_tuple.append((element["title"], element["url"]))
-    #     sorted(result, key=element["comments_count"], reverse=True)
-    #     print('list_tuple: ',
-    #         sorted(result, key=element["comments_count"], reverse=True)
-    #     )
+    max_count = [x['comments_count'] for x in result]
+    sorted_count = sorted(max_count, reverse=True)
+    limeted = sorted_count[:5]
+    print('comments_count: ', limeted)
 
-    # for element in result:
-    #     list_tuple.append((element["title"], element["url"]))
-    return list_tuple
+    return limeted
 
 
 # Requisito 11

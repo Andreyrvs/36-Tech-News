1d04f0e5ee847e1c6fb9e79d7c398a40dab5ee46
Andrey Visniewski <cortaesai@gmail.com>
2023-01-30 21:02:33 -0300

    chore: (Requisito 10) - ordena atraves da key comment_count

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -3,16 +3,25 @@ from tech_news.database import find_news
 
 # Requisito 10
 def top_5_news():
-    sorted_ratings = []
-
     result = find_news()
+    if len(result) < 5:
+        return result
+ 
+    # /** 
+    # max_count retirada de:
+    # Source:
+    # https://stackoverflow.com/a/73050/17053855
+    # */
+    newlist = sorted(result, key=lambda d: d['comments_count'], reverse=True)
+    limeted_newlist = newlist[:5]
 
-    max_count = [x['comments_count'] for x in result]
-    sorted_count = sorted(max_count, reverse=True)
-    limeted = sorted_count[:5]
-    print('comments_count: ', limeted)
+    list_tuple = []
+    for element in limeted_newlist:
+        list_tuple.append((element["title"], element["url"]))
 
-    return limeted
+    print("\n", list_tuple)
+
+    return list_tuple
 
 
 # Requisito 11
